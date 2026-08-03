61078afd2bc78ff7b57cd23f3cbf383c17c3b12d
Wesley Neves <wesleyneves675@gmail.com>
2026-08-03 15:27:59 -0400

    classe que calcula media das notas completa

--- src/MediaNotas.java
+++ src/MediaNotas.java
@@ -4,5 +4,38 @@ public class MediaNotas {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
+		double soma = 0;
+		int contador = 0;
+		
+		System.out.println("Informe as notas do aluno (ou digite um numero negativo para encerrar): ");
+		
+		while(true) {
+			System.out.print("Digite uma nota: ");
+			double nota = scanner.nextDouble();
+			
+			if(nota < 0) {
+				break;
+			}
+			
+			soma += nota;
+			contador++;
+		}
+		System.out.println("Resultado final: ");
+		
+		if(contador > 0) {
+			double media = soma / contador;
+		
+			System.out.printf("Media das notas: %.2f\n", media);
+			
+			if(media >= 7) {
+				System.out.println("Situação: Aprovado");
+			}else {
+				System.out.println("Situação: Reprovado");
+			}
+		}else {
+			System.out.println("Nenhuma nota valida digitada.");
+		}
+		
+		scanner.close();
 	}
 }
