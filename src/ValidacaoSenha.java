191b3cd1dc6b61a0e181f0ad582c148efb5c3c0a
Wesley Neves <wesleyneves675@gmail.com>
2026-08-03 14:59:31 -0400

    classes de validacao de senha completa

---
+++ src/ValidacaoSenha.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ValidacaoSenha {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String senha = "Java123";
+		String senhaPedida;
+		
+		
+		
+		do {
+			System.out.println("Digite a senha de aceso: ");
+			senhaPedida = scanner.nextLine();
+			
+			if(!senhaPedida.equals(senha)) {
+				System.out.println("Senha inválida, tente novamente.\n");
+			}
+			
+		} while(!senhaPedida.equals(senha));
+		
+		System.out.println("Acesso concedido!!");
+		
+		scanner.close();
+	}
+}
