122f6fd21c12be42c1821096b9d3c677384e3239
Wesley Neves <wesleyneves675@gmail.com>
2026-08-03 14:04:07 -0400

    classes tabuada dinamica completa e contador de pares e impares em construcao

---
+++ src/TabuadaDinamica.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TabuadaDinamica {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int numero;
+		
+		System.out.print("Informe um numero de 1 a 10: ");
+		numero = scanner.nextInt();
+		if(numero > 10 || numero < 1) {
+			System.out.println("Por favor informe um numero inteiro entre 1 e 10!");
+		} else {
+			for(int i = 1; i <= 10; i++) {
+				System.out.println(numero+" x " + i + " = " +(numero * i));
+			}
+		}
+	}
+}
