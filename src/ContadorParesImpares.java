8323cb1bba110041ed1f5152d61ed578dfd1635f
Wesley Neves <wesleyneves675@gmail.com>
2026-08-03 14:43:01 -0400

    classe contador completa

--- src/ContadorParesImpares.java
+++ src/ContadorParesImpares.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class ContadorParesImpares {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int pares = 0, impares = 0;
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.print("Digite o "+i+"°numero: ");
+			int numero = scanner.nextInt();
+			
+			if(numero % 2 == 0) {
+				pares++;
+			} else {
+				impares++;
+			}
+		}
+		
+		System.out.println("Quantidade de numeros pares: "+pares);
+		System.out.println("Quantidade de numeros impares: "+impares);
+	}
 }
